c2158efce6595103aa3b51099a8043b530c316ad
MarinRatkovic-mr <62936158+MarinRatkovic-mr@users.noreply.github.com>
2020-04-19 21:16:02 +0200

    Beta Verzija
    Ostalo mi je samo par grešaka koje moram popraviti i nači ih naravno.

---
+++ 2D Platformer/Assets/skripte/Novcic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Novcic : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            Destroy(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 2D Platformer/Assets/skripte/PadPlatforme.cs
@@ -0,0 +1,37 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PadPlatforme : MonoBehaviour
+{
+    public float fallDelay = 1f;
+    private Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Invoke("Fall", fallDelay);
+        }
+    }
+    void Fall()
+    {
+        rb2d.isKinematic = false;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+}
